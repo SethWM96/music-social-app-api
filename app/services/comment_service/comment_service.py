0385eeec21ca773beda486de58e56aa35e81884d
Nicholas Dowling <nicholasdowling@Nicholass-MBP.lan>
2024-03-07 18:48:30 -0800

    Add comment service

--- app/services/comment_service/comment_service.py
+++ app/services/comment_service/comment_service.py
@@ -1,6 +1,7 @@
 import comment_service_interface as comment_service_interface
 from models import Comment, comment_create
 from collections import List
+from models import Comment, CommentCreate, CommentUpdate
 
 class CommentService(comment_service_interface):
     def __init__(self, comment_repo):
@@ -15,23 +16,23 @@ class CommentService(comment_service_interface):
     def get_comments(self) -> List[Comment]:
         return self.comment_repo.get_all_comments()
 
-    # def create_comment(self, comment_create: CommentCreate) -> Comment:
-    #     # Validate comment_create data
-    #     # Optionally perform any business logic checks before creating the comment
-    #     comment = self.comment_repo.create_comment(comment_create)
-    #     return comment
+    def create_comment(self, comment_create: CommentCreate) -> Comment:
+        # Validate comment_create data
+        # Optionally perform any business logic checks before creating the comment
+        comment = self.comment_repo.create_comment(comment_create)
+        return comment
 
-    # def update_comment(self, comment_id: int, comment_update: CommentUpdate) -> Comment:
-    #     # Check if the comment exists
-    #     existing_comment = self.get_comment(comment_id)
+    def update_comment(self, comment_id: int, comment_update: CommentUpdate) -> Comment:
+        # Check if the comment exists
+        existing_comment = self.get_comment(comment_id)
         
-    #     # Update the existing comment with new data
-    #     updated_comment = self.comment_repo.update_comment(comment_id, comment_update)
-    #     return updated_comment
+        # Update the existing comment with new data
+        updated_comment = self.comment_repo.update_comment(comment_id, comment_update)
+        return updated_comment
 
-    # def delete_comment(self, comment_id: int) -> None:
-    #     # Check if the comment exists
-    #     existing_comment = self.get_comment(comment_id)
+    def delete_comment(self, comment_id: int) -> None:
+        # Check if the comment exists
+        existing_comment = self.get_comment(comment_id)
         
-    #     # Delete the comment
-    #     self.comment_repo.delete_comment(comment_id)
+        # Delete the comment
+        self.comment_repo.delete_comment(comment_id)
